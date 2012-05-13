6953fad46977977ae2667f735bd6ebab3c2fc583
Mendhak <smaltaf@msn.com>
2012-05-13 23:39:30 +0100

    Moved main functionality to statusicon.py

--- statusicon.py
+++ statusicon.py
@@ -2,72 +2,107 @@
 import dbus
 from gi.repository import Gio, GLib
 import gtk
-import traceback
+import subprocess
+import sys
 import starhscale
 
+import dbus.mainloop.glib
+import glib
 
 class StatusIcon:
 
     star = None
     starValue = 0
 
+    iconsPath = "/usr/share/icons/"
+    rhythmboxIcon = iconsPath + "hicolor/32x32/apps/rhythmbox.png"
+    playIcon = iconsPath + "gnome/32x32/actions/media-playback-start.png"
+
     def __init__(self):
+
         self.statusicon = gtk.StatusIcon()
-        self.statusicon.set_from_stock(gtk.STOCK_HOME)
-        self.statusicon.connect("popup-menu", self.right_click_event)
-        self.statusicon.set_tooltip("StatusIcon Example")
+        #self.statusicon.set_from_stock(gtk.STOCK_MEDIA_PLAY)
+        self.statusicon.set_from_file(self.rhythmboxIcon)
+        self.statusicon.connect("popup-menu", self.OnShowPopupMenu)
+        self.statusicon.connect("button-press-event", self.OnIconClick)
+        self.statusicon.connect("scroll-event", self.OnIconScroll)
 
         window = gtk.Window()
         window.connect("destroy", lambda w: gtk.main_quit())
         #window.show_all()
 
-    def get_song_uri(self):
+    def OnIconScroll(self, widget, event):
 
-        bus = dbus.SessionBus()
-        proxy = bus.get_object('org.mpris.MediaPlayer2.rhythmbox','/org/mpris/MediaPlayer2')
-        properties_manager = dbus.Interface(proxy, 'org.freedesktop.DBus.Properties')
-        currentSongURI = str(properties_manager.Get("org.mpris.MediaPlayer2.Player","Metadata").get(dbus.String(u'xesam:url')))
-        return currentSongURI
+        if event.direction == 0:
+            session_bus = dbus.SessionBus()
+            player = session_bus.get_object('org.mpris.MediaPlayer2.rhythmbox','/org/mpris/MediaPlayer2')
+            dbuspropiface = dbus.Interface(player,dbus_interface='org.freedesktop.DBus.Properties')
+            vol=dbuspropiface.Get("org.mpris.MediaPlayer2.Player","Volume")
+            vol+=(0.1*vol)
+            dbuspropiface.Set("org.mpris.MediaPlayer2.Player","Volume",vol)
+        elif event.direction == 1:
+            session_bus = dbus.SessionBus()
+            player = session_bus.get_object('org.mpris.MediaPlayer2.rhythmbox','/org/mpris/MediaPlayer2')
+            dbuspropiface = dbus.Interface(player,dbus_interface='org.freedesktop.DBus.Properties')
+            vol=dbuspropiface.Get("org.mpris.MediaPlayer2.Player","Volume")
+            vol-=(0.1*vol)
+            dbuspropiface.Set("org.mpris.MediaPlayer2.Player","Volume",vol)
 
-    def get_song_rating(self):
-        bus = dbus.SessionBus()
-        proxy = bus.get_object('org.mpris.MediaPlayer2.rhythmbox','/org/mpris/MediaPlayer2')
-        properties_manager = dbus.Interface(proxy, 'org.freedesktop.DBus.Properties')
-        currentRating = int(float(properties_manager.Get
-                    ('org.mpris.MediaPlayer2.Player','Metadata').get(dbus.String(u'xesam:userRating')))*5)
+    def OnIconClick(self, icon, event, data = None):
+        if event.button == 1: # left button
+            subprocess.call("rhythmbox")
+
+
+    def GetSongURI(self):
+        try:
+            bus = dbus.SessionBus()
+            proxy = bus.get_object('org.mpris.MediaPlayer2.rhythmbox','/org/mpris/MediaPlayer2')
+            properties_manager = dbus.Interface(proxy, 'org.freedesktop.DBus.Properties')
+            currentSongURI = str(properties_manager
+                        .Get("org.mpris.MediaPlayer2.Player","Metadata").get(dbus.String(u'xesam:url')))
+            return currentSongURI
+        except:
+            return None
+
+    def GetSongRating(self):
+        try:
+            bus = dbus.SessionBus()
+            proxy = bus.get_object('org.mpris.MediaPlayer2.rhythmbox','/org/mpris/MediaPlayer2')
+            properties_manager = dbus.Interface(proxy, 'org.freedesktop.DBus.Properties')
+            currentRating = int(float(properties_manager
+                        .Get('org.mpris.MediaPlayer2.Player','Metadata').get(dbus.String(u'xesam:userRating')))*5)
+        except:
+            currentRating = -1
 
         if currentRating:
             return currentRating
         else:
             return 0
 
-    def set_song_rating(self, rating):
-            tb = ''
+    def SetSongRating(self, rating):
+
             try:
-                #currentSongURI = shell.props.shell_player.get_playing_entry().get_playback_uri()
-                currentSongURI = self.get_song_uri()
+                currentSongURI = self.GetSongURI()
 
-                bus_type = Gio.BusType.SESSION
-                flags = 0
-                iface_info = None
+                if currentSongURI:
 
+                    busType = Gio.BusType.SESSION
+                    flags = 0
+                    ratingInterface = None
 
-                proxy = Gio.DBusProxy.new_for_bus_sync(bus_type, flags, iface_info,
-                                                       "org.gnome.Rhythmbox3",
-                                                       "/org/gnome/Rhythmbox3/RhythmDB",
-                                                       "org.gnome.Rhythmbox3.RhythmDB", None)
+                    proxy = Gio.DBusProxy.new_for_bus_sync(busType, flags, ratingInterface,
+                                                           "org.gnome.Rhythmbox3",
+                                                           "/org/gnome/Rhythmbox3/RhythmDB",
+                                                           "org.gnome.Rhythmbox3.RhythmDB", None)
 
-                vrating = GLib.Variant("d", float(rating))
-                proxy.SetEntryProperties("(sa{sv})", currentSongURI, {"rating": vrating})
+                    variantRating = GLib.Variant("d", float(rating))
+                    proxy.SetEntryProperties("(sa{sv})", currentSongURI, {"rating": variantRating})
             except:
-                tb = traceback.format_exc()
-            finally:
-                print tb
-
-            return False
+                print "Failed to set a rating"
 
 
-    def motion_notify_event(self, widget, event):
+    def OnRatingMouseOver(self, widget, event):
+        # User moving over the stars, highlight stars accordingly
         if event.is_hint:
            x, y, state = event.window.get_pointer()
         else:
@@ -76,64 +111,120 @@ class StatusIcon:
 
         self.star.check_for_new_stars(x-self.star.allocation.x)
 
-    def click_notify_event(self, widget, event):
+    def OnRatingClick(self, widget, event):
+        # User clicked on a star. Set the rating.
         event.x = event.x - self.star.allocation.x
         self.star.do_button_press_event(event)
+
         self.starValue = self.star.stars
-        self.set_song_rating(self.starValue)
+        self.SetSongRating(self.starValue)
 
-
-    def leave_notify_event(self, widget, event):
+    def OnRatingMouseOut(self, widget, event):
+        # Mouse left the star area, restore to original rating
         self.star.set_value(self.starValue)
 
-    def right_click_event(self, icon, button, time):
+    def OnPlayPauseClick(self, widget):
+        try:
+            session_bus = dbus.SessionBus()
+            player = session_bus.get_object('org.mpris.MediaPlayer2.rhythmbox','/org/mpris/MediaPlayer2')
+            playeriface = dbus.Interface(player, dbus_interface='org.mpris.MediaPlayer2.Player')
+            playeriface.PlayPause()
+        except:
+            print "Unable to play/pause, are you sure Rhythmbox is running?"
+
+
+    def OnNextClick(self, widget):
+        try:
+            session_bus = dbus.SessionBus()
+            player = session_bus.get_object('org.mpris.MediaPlayer2.rhythmbox','/org/mpris/MediaPlayer2')
+            playeriface = dbus.Interface(player, dbus_interface='org.mpris.MediaPlayer2.Player')
+            playeriface.Next()
+        except:
+            print "Unable to go to next item, are you sure Rhythmbox is running?"
+
+    def OnPreviousClick(self, widget):
+        try:
+
+            session_bus = dbus.SessionBus()
+            player = session_bus.get_object('org.mpris.MediaPlayer2.rhythmbox','/org/mpris/MediaPlayer2')
+            playeriface = dbus.Interface(player, dbus_interface='org.mpris.MediaPlayer2.Player')
+            playeriface.Previous()
+        except:
+            print "Unable to go to previous item, are you sure Rhythmbox is running?"
+
+    def OnQuitClick(self, widget):
+        try:
+            session_bus = dbus.SessionBus()
+            player = session_bus.get_object('org.mpris.MediaPlayer2.rhythmbox','/org/mpris/MediaPlayer2')
+            mplayeriface = dbus.Interface(player, dbus_interface='org.mpris.MediaPlayer2')
+            mplayeriface.Quit()
+        except:
+            sys.exit()
+
+
+    def OnPlaybackStatusChanged (self, sender, properties, sig):
+        if "PlaybackStatus" in properties:
+            print properties["PlaybackStatus"]
+
+
+    def SetupPlaybackStatusListener(self):
+        dbus.mainloop.glib.DBusGMainLoop (set_as_default = True)
+
+        bus = dbus.SessionBus ()
+
+        proxy = bus.get_object('org.mpris.MediaPlayer2.rhythmbox','/org/mpris/MediaPlayer2')
+        player = dbus.Interface(proxy, 'org.freedesktop.DBus.Properties')
+        player.connect_to_signal("PropertiesChanged", self.OnPlaybackStatusChanged)
+
+        mainloop = glib.MainLoop ()
+        mainloop.run ()
+
+    def OnShowPopupMenu(self, icon, button, time):
         menu = gtk.Menu()
 
-        about = gtk.MenuItem("About")
+        playpause = gtk.MenuItem("Play/Pause")
+        next = gtk.MenuItem("Next")
+        prev = gtk.MenuItem("Prev")
         quit = gtk.MenuItem("Quit")
 
-        ### MODIFIED PART!! ###
+        ratingItem = self.GetRatingWidget()
+        if ratingItem:
+            menu.append(ratingItem)
 
-        item = gtk.MenuItem()
+        playpause.connect("activate", self.OnPlayPauseClick)
+        next.connect("activate", self.OnNextClick)
+        prev.connect("activate", self.OnPreviousClick)
+        quit.connect("activate", self.OnQuitClick)
 
-
-        ####gobject.type_register(starhscale.StarHScale)
-
-        self.starValue =  self.get_song_rating()
-        self.star = starhscale.StarHScale(5, self.starValue)
-
-        item.add(self.star)
-        item.connect("motion_notify_event", self.motion_notify_event)
-        item.connect("button_press_event", self.click_notify_event)
-        item.connect("leave_notify_event", self.leave_notify_event)
-
-
-        style = item.get_style().copy()
-        item.set_style(style)
-
-        menu.append(item)
-        #### END MODIFIED PART ####
-
-        about.connect("activate", self.show_about_dialog)
-        quit.connect("activate", gtk.main_quit)
-
-        menu.append(about)
+        menu.append(playpause)
+        menu.append(next)
+        menu.append(prev)
         menu.append(quit)
         menu.show_all()
 
+        #TODO, get DBus listener working
+        #self.SetupPlaybackStatusListener()
 
         menu.popup(None, None, gtk.status_icon_position_menu, button, time, self.statusicon)
 
-    def show_about_dialog(self, widget):
-        about_dialog = gtk.AboutDialog()
+    def GetRatingWidget(self):
+        ratingItem = gtk.MenuItem()
+        self.starValue =  self.GetSongRating()
 
-        about_dialog.set_destroy_with_parent(True)
-        about_dialog.set_name("StatusIcon Example")
-        about_dialog.set_version("1.0")
-        about_dialog.set_authors(["Andrew Steele"])
+        if self.starValue >= 0:
+            self.star = starhscale.StarHScale(5, self.starValue)
 
-        about_dialog.run()
-        about_dialog.destroy()
+            ratingItem.add(self.star)
+            ratingItem.connect("motion_notify_event", self.OnRatingMouseOver)
+            ratingItem.connect("button_press_event", self.OnRatingClick)
+            ratingItem.connect("leave_notify_event", self.OnRatingMouseOut)
+
+            style = ratingItem.get_style().copy()
+            ratingItem.set_style(style)
+
+            return ratingItem
+        else:
+            return None
 
 StatusIcon()
 gtk.main()
